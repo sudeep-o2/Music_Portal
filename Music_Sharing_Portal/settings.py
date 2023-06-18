2f4f738553502f819b1e7e04af6ff8580fc4e6e1
Sudeep <sudeepsavai16@gmail.com>
2023-06-18 13:46:19 +0530

    modified for vercel

--- Music_Sharing_Portal/settings.py
+++ Music_Sharing_Portal/settings.py
@@ -140,7 +140,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_ROOT = BASE_DIR / 'static/music'
 
